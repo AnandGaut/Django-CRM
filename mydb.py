5bf566ea3154dc4866a36a2f735c289e3c5a943e
Anand Gaut <anandgautam1124@gmail.com>
2023-04-20 14:36:39 +0530

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+# Set Up database
+import mysql.connector
+
+dataBase =mysql.connector.connect(
+    host ='localhost',
+    user = 'root',
+    passwd = 'password123'
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE elderco")
+
+print("All Done!")
